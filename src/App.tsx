7a8889f7704e50f4d0450996ee5025d2d0376f7d
Ankan Maity <ankanmaity27@gmail.com>
2025-07-18 15:46:38 +0530

    portfolio data update

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import AboutSection from './sections/AboutSection';
 import SkillsSection from './sections/SkillsSection';
 import ProjectsSection from './sections/ProjectsSection';
 import ContactSection from './sections/ContactSection';
+import { portfolioData } from './Data/PortfolioData';
 
 type SectionName = 'home' | 'about' | 'skills' | 'projects' | 'contact';
 
@@ -58,106 +59,6 @@ const App = () => {
       });
     };
   }, []);
-
-  const portfolioData = {
-    name: "Ankan Maity",
-    profileImage: "/images/profile1.jpg",
-    tagline: "Full-Stack Developer | Passionate about creating impactful web experiences | IOT Enthusiast",
-    about: `Hello! I'm Ankan Maity, a passionate full-stack developer with a deep passion for building scalable, 
-    efficient, and impactful digital experiences. I enjoy turning complex problems into simple, 
-    elegant solutions — from designing responsive front-end interfaces to architecting robust backend systems.`,
-
-    skills: [
-      {
-        id:1,
-        title: "languages",
-        items: ["C/C++", "JavaScript", "TypeScript", "C#"]
-      },
-      {
-        id: 8,
-        title: "web technologies",
-        items: ["HTML", "CSS", "Tailwind CSS", "Bootstrap", "Material UI", "Shadcn UI"]
-      },
-      {
-        id:2,
-        title: "cloud",
-        items: ["AWS","Google Cloud"]
-      },
-      {
-        id:3,
-        title: "databases",
-        items: ["PostgreSQL", "MySQL"]
-      },
-      {
-        id:4,
-        title: "tools",
-        items: ["Git","Github", "Docker"]
-      },
-      {
-        id: 5,
-        title: "frameworks",
-        items: ["React", "Node.js", "Express.js"]
-      },
-      {
-        id: 6,
-        title: "IOT Boards",
-        items: ["Arduino Uno", "Esp32", "Raspberry Pi 5"]
-      },
-      {
-        id:7,
-        title: "other",
-        items: ["REST APIs", "Microservices", "IOT"]
-      }
-    ],
-
-    projects: [
-      {
-        id: 1,
-        title: "♻️ EcoBin–IoT-Based Smart Waste Management System",
-        description: ` EcoBin is a full-stack waste management solution
-          designed using a microservices architecture. The backend is
-          built with .NET Web API and PostgreSQL, while the
-          frontend uses React (TypeScript) and Tailwind CSS. The
-          system is containerized with Docker and implements
-          role-based authentication for Admins, Collectors, Users, and
-          Guests.`,
-        link: "https://github.com/Ankan5960/EcoBin",
-        image: "/images/ecobin.jpg"
-      },
-      {
-        id: 2,
-        title: "🗳️ Fingerprint-based-electronic-voting-machine",
-        description: `A Fingerprint
-          Based Electronic Voting Machine (EVM) integrated
-          biometric technology for enhanced security. Voters
-          authenticate their identity using fingerprints, ensuring that
-          only eligible voters can cast their votes. This system aims to
-          prevent electoral fraud, improve accuracy, and streamline the
-          voting process, making elections more secure and efficient`,
-        link: "https://github.com/Ankan5960/Fingerprint-based-electronic-voting-machine",
-        image: "https://github.com/Ankan5960/Fingerprint-based-electronic-voting-machine/raw/main/circuit-diagram/circuit.png"
-      },
-      {
-        id: 3,
-        title: "📡 Fronted Website for Business",
-        description: `A fronted website for
-          "Maity Enterprise" which is providing fiber optic connection
-          to users. Users can see their location on this website, and
-          also find the plans. New users can find the nearest Optical
-          splitters from it.
-          `,
-        link: "https://maity-enterprise.netlify.app/Home",
-        image: "/images/maityenterprice.png"
-      }
-    ],
-    contact: {
-      email: "maityankan55@gmail.com",
-      phone: "+91 8509640363",
-      github: "https://github.com/Ankan5960",
-      linkedin: "https://www.linkedin.com/in/ankan-maity-a1b44927a/",
-      twitter: "https://x.com/AnkanMa54350416",
-    }
-  };
 
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900 pb-20 md:pb-0">
---
+++ src/Data/PortfolioData.ts
@@ -0,0 +1,108 @@
+export const portfolioData = {
+    name: "Ankan Maity",
+    profileImage: "/images/profile/profile.jpg",
+    tagline: "Full-Stack Developer | Passionate about creating impactful web experiences | IOT Enthusiast",
+    about: `Hello! I'm Ankan Maity, a passionate full-stack developer with a deep passion for building scalable, 
+    efficient, and impactful digital experiences. I enjoy turning complex problems into simple, 
+    elegant solutions — from designing responsive front-end interfaces to architecting robust backend systems.`,
+
+    skills: [
+      {
+        id:1,
+        title: "languages",
+        items: ["C/C++", "JavaScript", "TypeScript", "C#"]
+      },
+      {
+        id: 8,
+        title: "web technologies",
+        items: ["HTML", "CSS", "Tailwind CSS", "Bootstrap", "Material UI", "Shadcn UI"]
+      },
+      {
+        id:2,
+        title: "cloud",
+        items: ["AWS","Google Cloud"]
+      },
+      {
+        id:3,
+        title: "databases",
+        items: ["PostgreSQL", "MySQL"]
+      },
+      {
+        id:4,
+        title: "tools",
+        items: ["Git","Github", "Docker"]
+      },
+      {
+        id: 5,
+        title: "frameworks",
+        items: ["React", "Node.js", "Express.js"]
+      },
+      {
+        id: 6,
+        title: "IOT Boards",
+        items: ["Arduino Uno", "Esp32", "Raspberry Pi 5"]
+      },
+      {
+        id:7,
+        title: "other",
+        items: ["REST APIs", "Microservices", "IOT"]
+      }
+    ],
+
+    projects: [
+      {
+        id: 1,
+        title: "♻️ EcoBin–IoT-Based Smart Waste Management System",
+        description: ` EcoBin is a full-stack waste management solution
+          designed using a microservices architecture. The backend is
+          built with .NET Web API and PostgreSQL, while the
+          frontend uses React (TypeScript) and Tailwind CSS. The
+          system is containerized with Docker and implements
+          role-based authentication for Admins, Collectors, Users, and
+          Guests.`,
+        link: "https://github.com/Ankan5960/EcoBin",
+        image: "/images/projects/ecobin.jpg"
+      },
+      {
+        id: 2,
+        title: "🗳️ Fingerprint-based-electronic-voting-machine",
+        description: `A Fingerprint
+          Based Electronic Voting Machine (EVM) integrated
+          biometric technology for enhanced security. Voters
+          authenticate their identity using fingerprints, ensuring that
+          only eligible voters can cast their votes. This system aims to
+          prevent electoral fraud, improve accuracy, and streamline the
+          voting process, making elections more secure and efficient`,
+        link: "https://github.com/Ankan5960/Fingerprint-based-electronic-voting-machine",
+        image: "https://github.com/Ankan5960/Fingerprint-based-electronic-voting-machine/raw/main/circuit-diagram/circuit.png"
+      },
+      {
+        id: 3,
+        title: "📡 Fronted Website for Business",
+        description: `A fronted website for
+          "Maity Enterprise" which is providing fiber optic connection
+          to users. Users can see their location on this website, and
+          also find the plans. New users can find the nearest Optical
+          splitters from it.
+          `,
+        link: "https://maity-enterprise.netlify.app/Home",
+        image: "/images/projects/maityenterprice.png"
+      },
+      {
+        id: 4,
+        title: " Some Entry level frontend project using HTML, CSS, JS",
+        description: ` some of my entry level projects using (html, css, js) its very helpful for newcomer, 
+        just make sure live preview is installed your vs code start extension, 
+        by defult web server is started in http://localhost:5500/ Now here is some projects`,
+        link: "https://github.com/Ankan5960/web-projects",
+        image: "/images/projects/webprojects.png"
+      }
+    ],
+    contact: {
+      email: "maityankan55@gmail.com",
+      phone: "+91 8509640363",
+      github: "https://github.com/Ankan5960",
+      linkedin: "https://www.linkedin.com/in/ankan-maity-a1b44927a/",
+      twitter: "https://x.com/AnkanMa54350416",
+    }
+  };
